acd21729d3a357fa4b7402ec7cbfb3ac9358d6bf
rebeccasiow <rebecca.siow@gmail.com>
2016-02-29 11:32:01 -0600

    Expense.swift

--- Payback/Expense.swift
+++ Payback/Expense.swift
@@ -13,21 +13,58 @@ class Expense {
     var name:String
     var payer:User
     var totalAmount:Double
+    var owers:[User]
     var amountOwed:[User : Double]
     var amountPaid:[User : Double]
     
-    init() {
+    init(id:Int, name:String, payer:User, totalAmount:Double, owers:[User], amountOwed:[User : Double],amountPaid:[User : Double]) {
+        self.id = id
+        self.name = name
+        self.payer = payer
+        self.totalAmount = totalAmount
+        self.owers = owers
+        self.amountOwed = amountOwed
+        self.amountPaid = amountPaid
+
+    }
+    
+    init(id:Int, name:String, payer:User, totalAmount:Double, owers:[User]) {
+        
+        self.id = id
+        self.name = name
+        self.payer = payer
+        self.totalAmount = totalAmount
+        self.owers = owers
+        let owerCount = owers.count
+        let autosplit = totalAmount/Double(owerCount)
+        for user in owers[1..<owerCount] {
+            amountOwed[user] = autosplit
+            amountPaid[user] = 0.0
+        }
         
     }
     
     
     func getAmountOwed(user: User) -> Double { // Returns -1 if user is not involved
-        
+        if let val = amountOwed[user] {
+            return amountOwed[user]!;
+        }
+        else{
+            return -1;
+        }        
     }
     func getAmountPaid(user: User) -> Double {
+        if let val = amountPaid[user] {
+            return amountPaid[user]!;
+        }
+        else{
+            return -1;
+        }
         
     }
     func getBalance(user: User) -> Double { //Same as getAmountOwed-getAmountPaid
+        
+        return amountOwed[user]!-amountPaid[user]!;
         
     }
 }
