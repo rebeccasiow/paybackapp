4ddeac3b20daaf547bbaeefdbe3f6b33f0747008
rebeccasiow <rebecca.siow@gmail.com>
2016-03-05 14:26:49 -0600

    some conflicts

--- Payback/AddExpenseViewController.swift
+++ Payback/AddExpenseViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class AddExpenseViewController: UIViewController {
-
+    @IBOutlet weak var itemField: UITextField!
+    @IBOutlet weak var amountField: UITextField!
+    @IBOutlet weak var buyerField: UITextField!
+    @IBOutlet weak var owerField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        /**
+        let user0 = User(id: 0, name: "Shefali")
+        let user1 = User(id: 1, name: "Becca")
+        let user2 = User(id: 2, name: "Kelly")
+        let user3 = User(id: 3, name: "Tess")
+        let user4 = User(id: 4, name: "Silas")
         // Do any additional setup after loading the view.
+        
+        
+        var shefalisUsers: [User] = [user0, user1, user2, user3, user4]
+        
+        var shefalisExpense = Expense(id: 0, name: "pencils", payer: user0, totalAmount: 5.0, owers: shefalisUsers))
+**/
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
